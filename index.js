1316e18b56c1b6ec404ee490b4d4a2efbc76d360
Ayim Nelson <ayimnelson01@gmail.com>
2022-07-23 18:37:04 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Doesn't work from here and I dont know why
+
+var body = document.body,
+    overlay = document.querySelector('.overlay'),
+    overlayBtts = document.querySelectorAll('button[class$="overlay"]');
+
+[].forEach.call(overlayBtts, function(btt) {
+
+    btt.addEventListener('click', function() {
+
+        /* Detect the button class name */
+        var overlayOpen = this.className === 'open-overlay';
+
+        /* Toggle the aria-hidden state on the overlay and the 
+           no-scroll class on the body */
+        overlay.setAttribute('aria-hidden', !overlayOpen);
+        body.classList.toggle('noscroll', overlayOpen);
+
+        /* On some mobile browser when the overlay was previously
+           opened and scrolled, if you open it again it doesn't 
+           reset its scrollTop property after the fadeout */
+        setTimeout(function() {
+            overlay.scrollTop = 0;
+        }, 1000)
+
+    }, false);
+
+});
